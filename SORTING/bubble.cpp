457781b75a106a6550aa4082389bf55bff076c0b
SAMIR24 <samirpatel2475@gmail.com>
2025-09-09 11:21:58 +0530

    Add files via upload

---
+++ SORTING/bubble.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << " Size of array ";
+    cin >> n;  
+
+    int arr[n];
+
+    cout << "Enter Numbers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];  
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
---
+++ SORTING/insertion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Size of array: ";
+    cin >> n; 
+
+    int arr[n]; 
+
+    cout << "Enter Numbers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];   
+    }
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
